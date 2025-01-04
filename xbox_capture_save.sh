01aa498badd7822fca6a6f19e12f6170309de5a4
daenomo <dwenomo@gmail.com>
2025-01-04 16:09:33 +0900

    少し試した感じ、 -nostdin だけでよさそうなので 一旦 -nostdin だけにしてしばらく様子見る

--- xbox_capture_save.sh
+++ xbox_capture_save.sh
@@ -10,8 +10,7 @@ ffmpegcmd="ffmpeg.exe"
 while true; do
 
   # 動画を移動してエンコードする
-  find "${onedirvedir}" -name "*.mp4" -print0 | while IFS= read -r -d '' file; do
-    sleep 1s
+  find "${onedirvedir}" -name "*.mp4" -print0 | while IFS= read -r -d '' file; do sleep 1s
     namewithext="${file#"${onedirvedir}"}"
     name=${namewithext%.mp4}
 
@@ -23,6 +22,7 @@ while true; do
 
     # Bluesky用
     ${ffmpegcmd} \
+      -nostdin \
       -i "${workdir}${namewithext}" \
       -vf "scale=-1:720" -c:v h264_amf -maxrate 7000k -c:a copy -b 7000k \
       -f segment \
@@ -32,9 +32,8 @@ while true; do
       -segment_time 55 \
       -loglevel verbose \
       "${forbsdir}${name}_bs_%02d.mp4"
+#      "${forbsdir}${name}_bs_%02d.mp4" </dev/null
     
-    sleep 1s
-
     # 元の動画も残しておく
     mv "${workdir}${namewithext}" "${forxdir}"
 
@@ -42,13 +41,11 @@ while true; do
 
   # スクリーンショットを移動
   find "${windowshomedir}OneDrive/Pictures/Xbox Screenshots/" -name "*.*" -print0 | while IFS= read -r -d '' file; do
-    sleep 1s
     mv "$file" "${workdir}" 2> /dev/null
   done
 
   # 古いファイルを削除
   find "${workdir}" -mtime +2 -type f -print0 | while IFS= read -r -d '' file; do
-    sleep 1s
     rm "$file"
   done
 
