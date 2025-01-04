2931ba4d9746e4a0930821fc228c6253f37e06e7
daenomo <dwenomo@gmail.com>
2025-01-04 21:42:45 +0900

    readでファイルディスクリプタを動的に割り当てるようにしてみた

--- xbox_capture_save.sh
+++ xbox_capture_save.sh
@@ -7,22 +7,21 @@ forxdir="${workdir}/tmp/"
 forbsdir="${workdir}/tmp/"
 ffmpegcmd="ffmpeg.exe"
 
-while true; do
+while true; do sleep 1s
 
   # 動画を移動してエンコードする
-  find "${onedirvedir}" -name "*.mp4" -print0 | while IFS= read -r -d '' file; do sleep 1s
+  while IFS= read -r -d '' -u ${FD} file; do
     namewithext="${file#"${onedirvedir}"}"
     name=${namewithext%.mp4}
 
     # OneDriveの動機が遅いとmvに失敗するためエラー出力抑制
     mv "$file" "${workdir}" 2> /dev/null
     if [ ! -e "${workdir}${namewithext}" ]; then
-      exit 0
+      continue
     fi
 
     # Bluesky用
     ${ffmpegcmd} \
-      -nostdin \
       -i "${workdir}${namewithext}" \
       -vf "scale=-1:720" -c:v h264_amf -maxrate 7000k -c:a copy -b 7000k \
       -f segment \
@@ -30,23 +29,21 @@ while true; do
       -segment_format_options movflags=+faststart \
       -reset_timestamps 1 \
       -segment_time 55 \
-      -loglevel verbose \
       "${forbsdir}${name}_bs_%02d.mp4"
-#      "${forbsdir}${name}_bs_%02d.mp4" </dev/null
     
     # 元の動画も残しておく
     mv "${workdir}${namewithext}" "${forxdir}"
 
-  done
+  done {FD}< <(find "${onedirvedir}" -name "*.mp4" -print0)
 
   # スクリーンショットを移動
-  find "${windowshomedir}OneDrive/Pictures/Xbox Screenshots/" -name "*.*" -print0 | while IFS= read -r -d '' file; do
+  while IFS= read -r -d '' -u ${FD} file; do
     mv "$file" "${workdir}" 2> /dev/null
-  done
+  done {FD}< <(find "${windowshomedir}OneDrive/Pictures/Xbox Screenshots/" -name "*.*" -print0)
 
   # 古いファイルを削除
-  find "${workdir}" -mtime +2 -type f -print0 | while IFS= read -r -d '' file; do
+  while IFS= read -r -d '' -u ${FD} file; do
     rm "$file"
-  done
+  done {FD}< <(find "${workdir}" -mtime +2 -type f -print0)
 
 done
